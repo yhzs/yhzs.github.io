f96b3255031ab70cd187fd1e5873880da6944871
Sheng Qing <qing.sheng@thomsonreuters.com>
2017-08-30 09:38:39 +0800

    add cname

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter, HashRouter, Route, NavLink } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+const Home = () => (
+    <div>home</div>
+);
+const About = () => (
+    <div>about</div>
+);
+ReactDOM.render(
+<BrowserRouter>
+    <div>
+    <NavLink exact={true} to="/"><button>home</button></NavLink >
+    <NavLink to="/about"><button>about</button></NavLink>
+    <Route exact={true} path="/" component={Home} />
+    <Route path="/about" component={About} />
+    </div>
+</BrowserRouter>
+    , document.getElementById('root')
+)
